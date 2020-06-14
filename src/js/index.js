b008bd37e99d49e25f9e2db24485930fc266477f
Marlena Śliwińska <marlenasliwinska@gmail.com>
2020-06-14 16:31:10 +0200

    new files

--- src/js/index.js
+++ src/js/index.js
@@ -3,3 +3,51 @@ import '../scss/main.scss';
 /* place your code below */
 
 console.log('HELLO 🚀')
+
+function Hello (imie, wiek)
+{
+    console.log (`Mam na ${imie} i mam ${wiek} lat`);
+   
+};
+
+
+Hello ('Marlena', 28);
+
+
+const deathStar = {
+diameter: 1200000,
+fire: (target) => {
+    console.log (`${trget} destroyed`) },
+    isOperating: true,
+    levels: 357,
+    name: 'Death Star',
+    population: 1000,
+}
+
+
+console.log (deathStar);
+
+const Marlena = {
+waga: 57,
+wiek: 28,
+name: 'Marlena',
+sex: 'woman',
+
+}
+
+console.log (`Mam na imię ${Marlena.name} i mam ${Marlena.wiek} lat` );
+
+
+const menu = document.querySelector ('.burger--js')
+
+
+menu.addEventListener ('click', () => {
+
+const marlenka = document.querySelector ('.navigation--js');
+marlenka.classList.toggle('hamburger');
+}
+);
+
+const age = document.querySelector ('.age--js')
+age.innerHTML ('')
+console.log (age.value);
--- webpack.common.js
+++ webpack.common.js
@@ -63,5 +63,29 @@ module.exports = {
       chunks: ["index", "another"],
       filename: "another.html",
     }),
+    new HtmlWebpackPlugin({
+      template: "./src/pages/nauka-html.html",
+      inject: true,
+      chunks: ["index", "another"],
+      filename: "nauka-html.html",
+    }),
+    new HtmlWebpackPlugin({
+      template: "./src/pages/nauka-css.html",
+      inject: true,
+      chunks: ["index", "another"],
+      filename: "nauka-css.html",
+    }),
+    new HtmlWebpackPlugin({
+      template: "./src/pages/nauka-js.html",
+      inject: true,
+      chunks: ["index", "another"],
+      filename: "nauka-js.html",
+    }),
+    new HtmlWebpackPlugin({
+      template: "./src/pages/inne.html",
+      inject: true,
+      chunks: ["index", "another"],
+      filename: "inne.html",
+    }),
   ],
 };
